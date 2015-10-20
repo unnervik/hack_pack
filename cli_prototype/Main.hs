ddedff240e5769933a69461860c2c06b3b7efb2b
kristian@purestyle.se <kristian@purestyle.se>
2015-10-20 22:56:18 +0200

    parser + interpreter for simple test specification language

--- cli_prototype/Main.hs
+++ cli_prototype/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.List
+import System.IO
 import TestSpecParser
 
 help :: String
@@ -14,6 +15,15 @@ splitBy delimiter = foldr f [[]]
     where f c l@(x:xs) | c == delimiter = []:l
                        | otherwise = (c:x):xs
 
+interpret :: [Action] -> IO ()
+interpret [] = return ()
+interpret (a:as) = do
+    putStrLn $ "Performing action "   ++ name a
+    putStrLn $ "    using hosts: "    ++ show (hosts a)
+    putStrLn $ "    at ports: "       ++ show [fst (ports a) .. snd (ports a)]
+    putStrLn $ "    with protocols: " ++ show (protocol a)
+    interpret as
+
 main :: IO ()
 main = do
     putStr help
@@ -23,6 +33,12 @@ main = do
             return ()
         ["load", filename] -> do
             putStrLn $ "Load testspec " ++ filename ++ " ..."
+            file <- readFile filename
+            case parseSpec file of
+                Left error ->
+                    putStrLn $ "Parse error: " ++ (show error)
+                Right actionList ->
+                    interpret actionList
             main
         ["help"] -> do
             putStr help
--- cli_prototype/TestSpecParser.hs
+++ cli_prototype/TestSpecParser.hs
@@ -1,30 +1,95 @@
 module TestSpecParser
     ( Protocol(..)
     , Action(..)
-    , p_action
+    , parseSpec
     ) where
 
-import Control.Applicative
+import Control.Applicative hiding (many, (<|>))
 import Text.ParserCombinators.Parsec
--- import ApplicativeParsec
 
-data Protocol = HTTP | FTP | SSH
-    deriving (Eq, Show)
+data Protocol = HTTP1 | HTTP2 | FTP | SSH
+    deriving (Eq, Read, Show)
 
 data Action = Action {
-      protocol :: Protocol
-    , range    :: [Int]
+      name     :: String
+    , protocol :: [Protocol]
+    , hosts    :: (String, String)
+    , ports    :: (Int, Int)
     } deriving (Eq, Show)
 
-hmm :: Action
-hmm = case parse p_action "unknown" "wdijidjawdiwj" of
-          Left x  -> error "No parse"
-          Right y -> y
+parseSpec :: String -> Either ParseError [Action]
+parseSpec str = parse p_spec "unknown" str
+
+eol :: Parser Char
+eol = char '\n'
+
+p_comment :: Parser ()
+p_comment = do
+    char '#'
+    manyTill anyChar eol
+    --many1 anyChar `endBy` (char '#')
+    return ()
+
+p_name :: Parser String
+p_name = do
+    char '-'
+    name <- many1 alphaNum
+    char '-'
+    return name
+
+p_array :: Parser [String]
+p_array = do
+    char '['
+    arr <- (many alphaNum) `sepBy` char ','
+    char ']'
+    return arr
+
+p_range :: Parser (String, String)
+p_range = do
+    char '['
+    pt1 <- many1 (try alphaNum <|> char '.')
+    string "->"
+    pt2 <- many1 (try alphaNum <|> char '.')
+    char ']'
+    return (pt1, pt2)
 
 p_action :: Parser Action
 p_action = do
-    x <- many1 alphaNum
-    return $ Action HTTP [1..3]
+    many p_comment
+    name <- p_name
+    spaces
+    string "protocol:"
+    spaces
+    ptcls <- p_array
+    spaces
+    string "address:"
+    spaces
+    (hostFrom, hostTo) <- p_range
+    spaces
+    string "ports:"
+    spaces
+    (portFrom, portTo) <- p_range
+    spaces
+    return $ Action name
+                    (map read ptcls)
+                    (hostFrom, hostTo)
+                    (read portFrom, read portTo)
+
+p_spec :: Parser [Action]
+p_spec = many p_action -- <* eof
 
 p_applicative :: Parser (String, String)
 p_applicative = (,) <$> many1 letter <*> (spaces *> many1 digit)
+
+test = parseSpec $ "#iojkijij\n" ++
+                   "#kkokok\n" ++
+                   "-name-\n" ++
+                   "protocol: [HTTP1,HTTP2]\n" ++
+                   "address: [192.168.0.1->192.168.0.10]\n" ++
+                   "ports: [3->4]\n\n" ++
+                   "#iojkijij\n" ++
+                   "#kkokok\n" ++
+                   "-name-\n" ++
+                   "protocol: [HTTP1,HTTP2]\n" ++
+                   "address: [192.168.0.1->192.168.0.10]\n" ++
+                   "ports: [3->4]"
