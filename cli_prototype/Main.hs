df286fe5f987964a67669174e65b1a1247edb793
kristian@purestyle.se <kristian@purestyle.se>
2015-10-20 20:23:35 +0200

    Parsec alive and kicking

--- cli_prototype/Main.hs
+++ cli_prototype/Main.hs
@@ -1,5 +1,29 @@
 module Main where
-    import TestSpecParser
+
+import Data.List
+import TestSpecParser
+
+help :: String
+help = "hack_pack Command Line Interface. Options:\n" ++
+       "    exit\n" ++
+       "    load - filename.testspec\n" ++
+       "    help - show this message\n"
+
+splitBy :: (Eq a) => a -> [a] -> [[a]]
+splitBy delimiter = foldr f [[]]
+    where f c l@(x:xs) | c == delimiter = []:l
+                       | otherwise = (c:x):xs
 
 main :: IO ()
-main = putStrLn "load file.testspec to carry out tests"
+main = do
+    putStr help
+    line <- getLine
+    case splitBy ' ' line of
+        ("exit" : _) ->
+            return ()
+        ["load", filename] -> do
+            putStrLn $ "Load testspec " ++ filename ++ " ..."
+            main
+        ["help"] -> do
+            putStr help
+            main
--- cli_prototype/TestSpecParser.hs
+++ cli_prototype/TestSpecParser.hs
@@ -4,14 +4,27 @@ module TestSpecParser
     , p_action
     ) where
 
-import ApplicativeParsec
+import Control.Applicative
+import Text.ParserCombinators.Parsec
+-- import ApplicativeParsec
 
 data Protocol = HTTP | FTP | SSH
+    deriving (Eq, Show)
 
 data Action = Action {
       protocol :: Protocol
     , range    :: [Int]
     } deriving (Eq, Show)
 
-p_action :: CharParser () Action
-p_action = undefined -- many1 char <* many1 ""
+hmm :: Action
+hmm = case parse p_action "unknown" "wdijidjawdiwj" of
+          Left x  -> error "No parse"
+          Right y -> y
+
+p_action :: Parser Action
+p_action = do
+    x <- many1 alphaNum
+    return $ Action HTTP [1..3]
+
+p_applicative :: Parser (String, String)
+p_applicative = (,) <$> many1 letter <*> (spaces *> many1 digit)
